de1cb48bf7b20d15770fa5bb8816a0d491364c0e
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2023-03-15 03:15:04 +0530

    first commit

---
+++ src/components/ScatterPlot.tsx
@@ -0,0 +1,65 @@
+
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+const ScatterPlot: React.FC<{ data: any[] }> = ({ data }) => {
+  const dataAll: any = data.map((item) => {
+    return [
+      { "name": "Alcohol", value: item["Alcohol"] },
+      { "name": "Malic Acid", value: item["Malic Acid"] },
+      { "name": "Ash", value: item["Ash"] },
+      { "name": "Alcalinity of ash", value: item["Alcalinity of ash"] },
+      { "name": "Magnesium", value: item["Magnesium"] },
+      { "name": "Total phenols", value: item["Total phenols"] },
+      { "name": "Flavanoids", value: item["Flavanoids"] },
+      { "name": "Nonflavanoid phenols", value: item["Nonflavanoid phenols"] },
+      { "name": "Proanthocyanins", value: item["Proanthocyanins"] },
+      { "name": "Color intensity", value: item["Color intensity"] },
+      { "name": "Hue", value: item["Hue"] },
+      { "name": "OD280/OD315 of diluted wines", value: item["OD280/OD315 of diluted wines"] },
+      { "name": "Unknown", value: item["Unknown"] },
+    ]
+  })
+  // const newdata: any = []
+  // console.log(newdata);
+
+  // for (let i of dataAll) {
+  //   newdata.push(i)
+
+  // }
+  // console.log(dataAll[0].map((item: any) => { return item.name }), "dataAll");
+
+  const option = {
+    title: {
+      text: 'ECharts Scatter'
+    },
+    tooltip: {},
+    legend: {
+      data: ['Scatter Chart']
+    },
+    xAxis: {
+      data: dataAll[0].map((item: any) => { return item.name })
+    },
+    yAxis: {},
+    series: [{
+      name: 'Scatter Chart',
+      type: 'scatter',
+      data: (function () {
+        var array = [];
+        var i
+        for (i = 0; i < dataAll.length; i++) {
+          array[i] = (Math.random() * 60);
+        }
+        return array;
+      })()
+    }]
+  };
+
+  return <ReactECharts
+    option={option}
+    style={{ height: 400 }}
+    opts={{ renderer: 'svg' }}
+  />;
+};
+
+export default ScatterPlot;
